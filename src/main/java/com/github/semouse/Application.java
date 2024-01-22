0e2170f75763712b018b4e76a44f007abf62f3e5
Leanid Barok <tratos149@gmail.com>
2024-01-22 13:17:41 +0300

    Setup executable jar build.

--- src/main/java/com/github/semouse/Application.java
+++ src/main/java/com/github/semouse/Application.java
@@ -1,9 +1,10 @@
-package src.main.java.com.github.semouse;
+package com.github.semouse;
 
 import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("Hello world");
         Arrays.stream(args).forEach(System.out::println);
     }
 }
